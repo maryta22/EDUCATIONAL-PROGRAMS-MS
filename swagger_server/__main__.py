ecf955edceead2b0269c1827fc0432934e954a88
maryta22 <maria.mariaog.rivera@gmail.com>
2025-01-29 01:55:54 -0500

    dockerfile

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,8 +9,9 @@ from swagger_server import encoder
 def main():
     app = FlaskApp(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'EDUCATIONAL-PROGRAMS-MS'}, pythonic_params=True)
-    app.run(port=2055)
+    CORS(app.app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+    app.add_api('swagger.yaml', arguments={'title': 'SELLERS-MS'}, pythonic_params=True)
+    app.run(host='0.0.0.0', port=2055)
 
 
 if __name__ == '__main__':
